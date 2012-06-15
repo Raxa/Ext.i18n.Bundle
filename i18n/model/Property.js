87ef854c68a095c5f63fd54f1417e5b723005e9e
Tarang <tarangmahajan@gmail.com>
2012-06-15 10:52:18 +0530

    Code Beautified and deleted irrelevant files.

--- i18n/model/Property.js
+++ i18n/model/Property.js
@@ -1,7 +1,8 @@
-genericConfig = {
-    extend: 'Ext.data.Model',
-    idProperty: 'key',
-    fields: ['key', 'value']
-};
+Ext.define('Ext.i18n.model.Property', {
+	extend: 'Ext.data.Model',
+	
+	idProperty: 'key',
+	fields: ['key', 'value']
+		
 
-Ext.define('Ext.i18n.model.Property', Util.platformizeModelConfig(genericConfig));
+});
